0fcad46fe91f20ee82486318dc56694aeb9f7f90
Kevin Fodness <kevin@alleyinteractive.com>
2017-03-03 14:58:38 -0500

    Revert "OPT Refactored I18N class to use singleton pattern and standard hook registration."
    This reverts commit c395847891c6188abc69510b82f43fb32902b7b0.

--- includes/class-i18n.php
+++ includes/class-i18n.php
@@ -19,86 +19,16 @@ namespace Optimizely_X;
 class I18N {
 
 	/**
-	 * Singleton instance.
-	 *
-	 * @since 1.0.0
-	 * @access private
-	 * @var I18N
-	 */
-	private static $instance;
-
-	/**
-	 * Gets the singleton instance.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 *
-	 * @return I18N
-	 */
-	public static function instance() {
-
-		// Initialize the instance, if necessary.
-		if ( ! isset( self::$instance ) ) {
-			self::$instance = new I18N;
-			self::$instance->setup();
-		}
-
-		return self::$instance;
-	}
-
-	/**
 	 * Load the plugin text domain for translation.
 	 *
 	 * @since 1.0.0
 	 * @access public
 	 */
-	public function load_plugin_textdomain() {
+	public static function load_plugin_textdomain() {
 		load_plugin_textdomain(
 			'optimizely-x',
 			false,
 			OPTIMIZELY_X_BASE_DIR . '/languages'
 		);
-	}
-
-	/**
-	 * Empty clone method, forcing the use of the instance() method.
-	 *
-	 * @see self::instance()
-	 *
-	 * @access private
-	 */
-	private function __clone() {
-	}
-
-	/**
-	 * Empty constructor, forcing the use of the instance() method.
-	 *
-	 * @see self::instance()
-	 *
-	 * @access private
-	 */
-	private function __construct() {
-	}
-
-	/**
-	 * Empty wakeup method, forcing the use of the instance() method.
-	 *
-	 * @see self::instance()
-	 *
-	 * @access private
-	 */
-	private function __wakeup() {
-	}
-
-	/**
-	 * Registers action and filter hooks.
-	 *
-	 * @since 1.0.0
-	 * @access private
-	 */
-	private function setup() {
-
-		// Register action hooks.
-		add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
 	}
 }
