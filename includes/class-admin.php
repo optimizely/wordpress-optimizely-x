11cc00cbe58ba30a36f3f2aa78787ded171b70a3
Kevin Fodness <kevin@kevinfodness.com>
2017-04-18 13:28:32 -0400

    OPT New default variation template from the client. (#13)
    * Updates the default variation template to include support for title tags in both H1s and H2s.

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -35,8 +35,8 @@ JAVASCRIPT;
 	 */
 	const DEFAULT_VARIATION_TEMPLATE = <<<JAVASCRIPT
 var utils = window['optimizely'].get('utils');
-utils.waitForElement( '.post-\$POST_ID h1' ).then( function () {
-    var element = document.querySelector( '.post-\$POST_ID h1' );
+utils.waitForElement( '.post-\$POST_ID h1, .post-\$POST_ID h2' ).then( function () {
+    var element = document.querySelector( '.post-\$POST_ID h1, .post-\$POST_ID h2' );
     element.innerHTML = '\$NEW_TITLE';
 } );
 JAVASCRIPT;
